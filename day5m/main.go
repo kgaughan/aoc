ddc3368f03aa1e90a7564f090ed8f6b9b5ec9a39
Keith Gaughan <k@stereochro.me>
2015-12-20 22:18:58 +0000

    Day 5, part 1.

---
+++ day5m/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"aoc/day5"
+)
+
+func main() {
+	flag.Parse()
+
+	path := flag.Arg(0)
+	if path == "" {
+		log.Fatal("Provide at least one input file path")
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	nNice := 0
+	nTotal := 0
+	for scanner.Scan() {
+		nTotal++
+		if day5.IsNice(scanner.Text()) {
+			nNice++
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Nice entries: %v out of %v\n", nNice, nTotal)
+}

fa2c8a1d8901a939243dfa16130ee437146c59a0
Keith Gaughan <k@stereochro.me>
2015-12-20 19:05:14 +0000

    My Advent of Code answers up until now.

---
+++ day4/adventcoin.go
@@ -0,0 +1,24 @@
+package day4
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func AdventCoin(prefix string, nZeroes, startFrom, endAt int) int {
+	hexPrefix := strings.Repeat("0", nZeroes)
+
+	for i := startFrom; i <= endAt; i++ {
+		h := md5.New()
+		io.WriteString(h, prefix)
+		io.WriteString(h, fmt.Sprint(i))
+
+		hex := fmt.Sprintf("%x", h.Sum(nil))
+		if strings.HasPrefix(hex, hexPrefix) {
+			return i
+		}
+	}
+	return 0
+}

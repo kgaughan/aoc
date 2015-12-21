6ffa2d0269bfb90dbab3fc34491a4ee76c0c7ebd
Keith Gaughan <k@stereochro.me>
2015-12-21 02:07:20 +0000

    Day 5, part 2.

--- day5/niceness.go
+++ day5/niceness.go
@@ -44,3 +44,31 @@ func IsNice1(s string) bool {
 
 	return nVowels >= 3 && hasNiceSeq
 }
+
+// Check if the string contains a sequence of two characters two or more
+// times without overlap.
+func hasDupes(rs []rune) bool {
+	for i := 0; i < len(rs)-2; i++ {
+		for j := i + 2; j < len(rs)-1; j++ {
+			if rs[i] == rs[j] && rs[i+1] == rs[j+1] {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Check for pairs of identical characters separated by another character.
+func hasTwins(rs []rune) bool {
+	for i := 0; i < len(rs)-2; i++ {
+		if rs[i] == rs[i+2] {
+			return true
+		}
+	}
+	return false
+}
+
+func IsNice2(s string) bool {
+	rs := []rune(s)
+	return len(rs) > 3 && hasDupes(rs) && hasTwins(rs)
+}

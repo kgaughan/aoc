b1df3d7281b19f7d4c30b7269a2e9c3fb8d34222
Keith Gaughan <k@stereochro.me>
2015-12-27 00:49:31 +0000

    Break logic.go apart.

---
+++ day7/literal.go
@@ -0,0 +1,17 @@
+package day7
+
+import (
+	"fmt"
+)
+
+type Literal struct {
+	v Value
+}
+
+func (e Literal) String() string {
+	return fmt.Sprintf("%v", e.v)
+}
+
+func (e Literal) Evaluate(ctx Context) Value {
+	return e.v
+}

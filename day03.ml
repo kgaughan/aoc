d112be8f0184bbda969461d015a1a786c3c3c5a5
Keith Gaughan <k@stereochro.me>
2016-12-28 00:51:08 +0000

    Day 3, part 2.

--- day03.ml
+++ day03.ml
@@ -1,5 +1,7 @@
 #!/usr/bin/env ocaml
 
+exception Unexpected_length
+
 let is_valid_triangle (a, b, c) =
   (* Partly sort the numbers so the biggest comes last. *)
   let a', x = (min a b), (max a b) in
@@ -17,18 +19,28 @@ let read_file filename =
 let filter_valid_triangles =
   List.filter is_valid_triangle
 
-let count_valid_triangles lst =
+let count_triangles lst =
   List.length (filter_valid_triangles lst)
 
-let count_triangles_in filename =
-  count_valid_triangles (read_file filename)
+let rejig_triples lst =
+  let rec loop acc = function
+    | [] -> acc
+    | (a1, a2, a3) :: (b1, b2, b3) :: (c1, c2, c3) :: rst ->
+        loop ((a1, b1, c1) :: (a2, b2, c2) :: (a3, b3, c3) :: acc) rst
+    | _ -> raise Unexpected_length
+  in
+  loop [] lst
 
 let () =
   let src = ref "" in
+  let part2 = ref false in
   let speclist = [
     ("-f", Arg.Set_string src, "File to read from");
+    ("-2", Arg.Set part2, "Run part 2");
   ] in
   Arg.parse speclist print_endline "Usage:";
   if !src = ""
   then exit 1
-  else Printf.printf "Valid triangles: %d\n" (count_triangles_in !src)
+  else let lst = read_file !src in
+       let jigged = if !part2 then rejig_triples lst else lst in
+       Printf.printf "Valid triangles: %d\n" (count_triangles jigged)

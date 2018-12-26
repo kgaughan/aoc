d29ffd81beabd42d3eaf72afe58bdbd9c1c1e468
Keith Gaughan <k@stereochro.me>
2018-12-26 23:07:13 +0000

    Day 1; part 1 solution.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("input.txt")?;
+    let reader = BufReader::new(f);
+
+    let mut total: i32 = 0;
+    for line in reader.lines() {
+        total += line?.parse::<i32>().unwrap()
+    }
+    println!("{}", total);
+
+    Ok(())
 }

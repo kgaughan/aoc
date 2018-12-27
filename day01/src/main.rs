71937024a541076d2a855a9dda349fb2fc99e2b9
Keith Gaughan <k@stereochro.me>
2018-12-27 04:28:19 +0000

    Day 1, part 2 solution.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,17 +1,44 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::vec;
 
 fn main() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let reader = BufReader::new(f);
 
     let mut total: i32 = 0;
+    let mut values: vec::Vec<i32> = vec![];
     for line in reader.lines() {
-        total += line?.parse::<i32>().unwrap()
+        let value: i32 = line?.parse().unwrap();
+        values.push(value);
+        total += value;
     }
-    println!("{}", total);
+
+    let mut first_repeated: i32 = 0;
+    let mut reached: HashSet<i32> = HashSet::new();
+    let mut found = false;
+    let mut freq = 0;
+    while !found {
+        for value in &values {
+            reached.insert(freq);
+            freq += value;
+            if reached.contains(&freq) {
+                found = true;
+                first_repeated = freq;
+                break;
+            }
+        }
+    }
+
+    println!("Final frequency: {}", total);
+    if found {
+        println!("First repeated: {}", first_repeated);
+    } else {
+        println!("No repetition found")
+    }
 
     Ok(())
 }

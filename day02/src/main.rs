4fd1e61e1bf1a5eff3931e3df478947ef355629a
Keith Gaughan <k@stereochro.me>
2018-12-27 15:08:26 +0000

    Finish analyse_box_id.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -8,15 +10,36 @@ struct Characteristics {
     has_three: bool,
 }
 
-fn analyse_box_id(_s: &str) -> Characteristics {
-    return Characteristics {
-        has_two: false,
-        has_three: false,
+fn analyse_box_id(s: &str) -> Characteristics {
+    let mut character_counts: HashMap<char, i32> = HashMap::new();
+
+    for ch in s.chars() {
+        let counter = character_counts.entry(ch).or_insert(0);
+        *counter += 1
     }
+
+    let mut result = Characteristics { has_two: false, has_three: false };
+    for n in character_counts.values() {
+        if *n == 2 {
+            result.has_two = true
+        } else if *n == 3 {
+            result.has_three = true
+        }
+    }
+
+    result
 }
 
 #[test]
 fn tests_box_id() {
+    assert_eq!(analyse_box_id("ab"),
+               Characteristics { has_two: false, has_three: false });
+    assert_eq!(analyse_box_id("aabb"),
+               Characteristics { has_two: true, has_three: false });
     assert_eq!(analyse_box_id("aabbb"),
-               Characteristics { has_two: true, has_three: false })
+               Characteristics { has_two: true, has_three: true });
+    assert_eq!(analyse_box_id("aaabbb"),
+               Characteristics { has_two: false, has_three: true });
+    assert_eq!(analyse_box_id("aaaabbbb"),
+               Characteristics { has_two: false, has_three: false });
 }

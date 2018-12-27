fe1ee1502664a1bc122a8bb090ba8ee5fcafe1ca
Keith Gaughan <k@stereochro.me>
2018-12-27 14:26:21 +0000

    Day 2, part 1: intentionally failing test.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(PartialEq, Debug)]
+struct Characteristics {
+    has_two: bool,
+    has_three: bool,
+}
+
+fn analyse_box_id(_s: &str) -> Characteristics {
+    return Characteristics {
+        has_two: false,
+        has_three: false,
+    }
+}
+
+#[test]
+fn tests_box_id() {
+    assert_eq!(analyse_box_id("aabbb"),
+               Characteristics { has_two: true, has_three: false })
+}

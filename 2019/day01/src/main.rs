68f53231d6159972bbcaf5eaec9004e3845376f1
Keith Gaughan <k@stereochro.me>
2019-12-02 00:07:22 +0000

    Restore part 1 solution.

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -4,8 +4,12 @@ use std::io::BufReader;
 use std::fs::File;
 
 fn get_fuel(mass: f64) -> f64 {
+    ((mass / 3.0).floor() - 2.0).max(0.0)
+}
+
+fn get_total_fuel(mass: f64) -> f64 {
     fn get_fuel_rec(mass: f64, total: f64) -> f64 {
-        let additional = ((mass / 3.0).floor() - 2.0).max(0.0);
+        let additional = get_fuel(mass);
         if additional > 0.0 {
             get_fuel_rec(additional, total + additional)
         } else {
@@ -15,15 +19,18 @@ fn get_fuel(mass: f64) -> f64 {
     get_fuel_rec(mass, 0.0)
 }
 
-fn main() -> io::Result<()> {
+fn calc(func: fn(f64) -> f64) -> io::Result<f64> {
     let f = File::open("input.txt")?;
 
     let total: f64 = BufReader::new(f)
         .lines()
         .map(|l| l.unwrap().parse::<f64>().unwrap())
-        .map(get_fuel).sum();
+        .map(func).sum();
 
-    println!("Fuel requirement: {}", total);
+    Ok(total)
+}
 
-    Ok(())
+fn main() {
+    println!("Part one: {}", calc(get_fuel).unwrap());
+    println!("Part two: {}", calc(get_total_fuel).unwrap());
 }

6bd811271a2fb3ebe2ed527d75f09160d63d4e35
Keith Gaughan <k@stereochro.me>
2019-12-03 15:53:40 +0000

    Day 2, part 2: solution

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
 
 fn load_intcode() -> io::Result<Vec<usize>> {
     let f = File::open("input.txt")?;
@@ -39,16 +40,54 @@ fn execute(intcode: &mut Vec<usize>) {
     }
 }
 
+const P2_TARGET: usize = 19690720;
+
 fn main() -> io::Result<()> {
-    let mut intcode = load_intcode().unwrap();
+    let intcode = load_intcode().unwrap();
 
-    // Restore 1202 program alarm
-    intcode[1] = 12;
-    intcode[2] = 2;
+    // Part one:
+    {
+        let mut intcode_dup = intcode.clone();
 
-    execute(&mut intcode);
+        // Restore 1202 program alarm
+        intcode_dup[1] = 12;
+        intcode_dup[2] = 2;
 
-    println!("Position 0: {}", intcode[0]);
+        execute(&mut intcode_dup);
+
+        println!("Part 1: {}", intcode_dup[0]);
+    }
+
+    // Part 2:
+    // This bruteforces the solution, but, as an optimisation, will skip
+    // anything where the target value of the noun and verb has been seen
+    // before.
+    let mut nouns_seen = HashSet::new();
+    'outer: for noun in 0..100 {
+        if nouns_seen.contains(&intcode[noun]) {
+            continue;
+        }
+        nouns_seen.insert(intcode[noun]);
+
+        let mut verbs_seen = HashSet::new();
+        for verb in 0..100 {
+            if verbs_seen.contains(&intcode[verb]) {
+                continue;
+            }
+            verbs_seen.insert(intcode[verb]);
+
+            let mut intcode_dup = intcode.clone();
+
+            intcode_dup[1] = noun;
+            intcode_dup[2] = verb;
+            execute(&mut intcode_dup);
+
+            if intcode_dup[0] == P2_TARGET {
+                println!("Part 2: {}", 100 * intcode_dup[1] + intcode_dup[2]);
+                break 'outer;
+            }
+        }
+    }
 
     Ok(())
 }

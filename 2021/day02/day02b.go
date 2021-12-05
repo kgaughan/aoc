56d6ed0cd5ea32532ae6b647bfa290d38a254674
Keith Gaughan <keith@gaughan.ie>
2021-12-05 17:55:34 +0000

    2021: day 2

---
+++ 2021/day02/day02b.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	var depth int64 = 0
+	var distance int64 = 0
+	var aim int64 = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, " ", 2)
+		amount, err := strconv.ParseInt(parts[1], 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch parts[0] {
+		case "forward":
+			distance += amount
+			depth += aim * amount
+		case "up":
+			aim -= amount
+		case "down":
+			aim += amount
+		}
+	}
+	fmt.Printf("Product: %v\n", depth*distance)
+}

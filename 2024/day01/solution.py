a58fc510ae3eda49a6c8b1eb314a29f5a92b36a2
Keith Gaughan <k@stereochro.me>
2024-12-01 17:17:50 +0000

    2024, day 1 (Python)

---
+++ 2024/day01/solution.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import collections
+import re
+
+
+def main():
+    lhs = []
+    rhs = []
+    with open("input") as fh:
+        for line in fh:
+            match = re.match(r"^(?P<lhs>\d+)\s+(?P<rhs>\d+)", line)
+            lhs.append(int(match["lhs"]))
+            rhs.append(int(match["rhs"]))
+
+    # Part 1:
+    part1 = sum(abs(r - l) for l, r in zip(sorted(lhs), sorted(rhs)))
+    print("Part 1:", part1)
+
+    # Part 2:
+    counts = collections.defaultdict(int)
+    for n in rhs:
+        counts[n] += 1
+    part2 = sum(n * counts.get(n, 0) for n in lhs)
+    print("Part 2:", part2)
+
+
+if __name__ == "__main__":
+    main()

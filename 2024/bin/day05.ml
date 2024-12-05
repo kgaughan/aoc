c55c6e1aab05727a2bdc815e5d26d807b58ed559
Keith Gaughan <k@stereochro.me>
2024-12-05 13:37:10 +0000

    2024, day 5, part 1: reuse check from part 2

--- 2024/bin/day05.ml
+++ 2024/bin/day05.ml
@@ -14,33 +14,24 @@ let read_input path =
   in
   In_channel.with_open_text path In_channel.input_lines |> parse
 
-let intersecting l1 l2 =
-  List.exists (fun x -> List.exists (fun y -> y = x) l2) l1
+let must_precede first second =
+  List.exists (fun entry -> entry = (second, first))
 
-let is_valid_update orderings =
-  let precedes_in_order page following =
-    let expected_predecessors = List.filter (fun (_, y) -> y = page) orderings |> List.map fst in
-    not (intersecting following expected_predecessors)
-  in
-  let rec in_order = function
-    | page :: tl -> 
-      if precedes_in_order page tl then
-        in_order tl
-      else
-        false
-    | [] -> true
-  in in_order
+let rec is_in_order orderings = function
+  | page :: tl ->
+    List.for_all (fun following -> must_precede following page orderings) tl &&
+    is_in_order orderings tl
+  | [] -> true
 
 let fix_order orderings =
-  let must_precede first second = List.exists (fun entry -> entry = (second, first)) orderings in
-  List.sort (fun a b -> if must_precede a b then -1 else 1)
+  List.sort (fun a b -> if must_precede a b orderings then -1 else 1)
 
 let get_middle_entry lst =
   List.nth lst ((List.length lst) / 2)
 
 let _ =
   let orderings, updates = read_input "input/day05.txt" in
-  let valid_updates, invalid_updates = List.partition (is_valid_update orderings) updates in
+  let valid_updates, invalid_updates = List.partition (is_in_order orderings) updates in
   let sum_middle_entries updates = List.map get_middle_entry updates |> List.fold_left (+) 0 in
   let part1 = sum_middle_entries valid_updates
   and part2 = List.map (fix_order orderings) invalid_updates |> sum_middle_entries in

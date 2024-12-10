acb2bdb4bfa345a3386a780722be00019aeb8d1e
Keith Gaughan <k@stereochro.me>
2024-12-10 02:35:53 +0000

    2024, day 9: this one is honestly embarassing

---
+++ 2024/bin/day09.ml
@@ -0,0 +1,115 @@
+let read_input path = In_channel.with_open_text path In_channel.input_line |> Option.value ~default:""
+
+type block =
+  | Block of int
+  | Empty
+
+let parse_digit ch =
+  let zero = int_of_char '0' in
+  int_of_char ch - zero
+
+let count_blocks = String.fold_left (fun acc ch -> acc + parse_digit ch) 0
+
+let populate_blocks disc_map =
+  let block_count = count_blocks disc_map in
+  let blocks = Array.make block_count Empty in
+  let offset = ref 0 in
+  let collect i ch =
+    let length = parse_digit ch in
+    if i mod 2 = 0 then
+      for j = !offset to !offset + length - 1 do
+        blocks.(j) <- Block (i / 2)
+      done;
+    offset := !offset + length
+  in
+  String.iteri collect disc_map;
+  blocks
+
+let pack_aggressively blocks =
+  let duplicate = Array.make (Array.length blocks) Empty in
+  let i = ref 0
+  and j = ref (Array.length blocks - 1) in
+  while !i <= !j do
+    if blocks.(!i) <> Empty then (
+      duplicate.(!i) <- blocks.(!i);
+      i := !i + 1)
+    else (
+      if blocks.(!j) <> Empty then (
+        duplicate.(!i) <- blocks.(!j);
+        i := !i + 1);
+      j := !j - 1)
+  done;
+  duplicate
+
+(* this is awful *)
+let pack_carefully blocks =
+  let dup = Array.copy blocks in
+  let i = ref 0
+  and j = ref (Array.length blocks - 1)
+  and gap = ref 0
+  and suitable = ref false
+  and file_size = ref 0 in
+  let last_id = ref Int.max_int in
+  while !j > 0 do
+    (* get the size of the file at the top *)
+    file_size := 1;
+    while !j > 0 && dup.(!j) = dup.(!j - 1) do
+      file_size := !file_size + 1;
+      j := !j - 1
+    done;
+    if
+      !j > 0
+      &&
+      match dup.(!j) with
+      | Block id -> id < !last_id
+      | Empty -> false
+    then (
+      (* scan from the bottom for a suitable gap *)
+      i := 0;
+      suitable := false;
+      while !i < !j && not !suitable do
+        while dup.(!i) <> Empty do
+          i := !i + 1
+        done;
+        (* find the size of the gap *)
+        gap := 0;
+        while !i + !gap < !j && dup.(!i + !gap) = Empty do
+          gap := !gap + 1
+        done;
+        if !gap >= !file_size then
+          suitable := true
+        else
+          i := !i + !gap
+      done;
+      if !suitable then
+        for k = 0 to !file_size - 1 do
+          dup.(!i + k) <- blocks.(!j + k);
+          dup.(!j + k) <- Empty
+        done;
+      (match dup.(!j) with
+      | Block id -> last_id := id
+      | Empty -> last_id := Int.max_int);
+      (* Skip to the next file *)
+      j := !j - 1;
+      while dup.(!j) = Empty do
+        j := !j - 1
+      done)
+    else
+      j := !j - 1
+  done;
+  dup
+
+let checksum blocks =
+  let sum_block (i, acc) block =
+    match block with
+    | Block id -> (i + 1, acc + (i * id))
+    | Empty -> (i + 1, acc)
+  in
+  snd (Array.fold_left sum_block (0, 0) blocks)
+
+let _ =
+  let disc_map = read_input "input/day09.txt" in
+  let blocks = populate_blocks disc_map in
+  let part1 = pack_aggressively blocks |> checksum in
+  let part2 = pack_carefully blocks |> checksum in
+  Printf.printf "Part 1: %d; Part 2: %d\n" part1 part2

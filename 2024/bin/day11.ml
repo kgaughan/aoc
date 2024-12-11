19eb038410a1f28e8192ce615ddaf81c5ede7068
Keith Gaughan <k@stereochro.me>
2024-12-11 07:07:48 +0000

    2024, day 11, part 2: got ahead of myself - needed to heavily memoise the calculations

--- 2024/bin/day11.ml
+++ 2024/bin/day11.ml
@@ -5,35 +5,31 @@ let split_number n =
   let half = String.length as_string / 2 in
   (int_of_string (String.sub as_string 0 half), int_of_string (String.sub as_string half half))
 
-let rec process n tiles =
-  let rec transform tiles acc =
-    match tiles with
-    | 0 :: tl -> transform tl (1 :: acc)
-    | n :: tl when has_even_number_of_digits n ->
-        let (a, b) = split_number n in
-        transform tl (a :: b :: acc)
-    | n :: tl -> transform tl ((n * 2024) :: acc)
-    | [] -> acc
+let process n tiles =
+  let cache = Hashtbl.create 100000 in
+  let rec transform tile n =
+    if n = 0 then
+      1
+    else
+      match Hashtbl.find_opt cache (tile, n) with
+      | Some r -> r
+      | None ->
+          let result =
+            if tile = 0 then
+              transform 1 (n - 1)
+            else if has_even_number_of_digits tile && tile > 9 then
+              let (a, b) = split_number tile in
+              transform a (n - 1) + transform b (n - 1)
+            else
+              transform (tile * 2024) (n - 1)
+          in
+          Hashtbl.add cache (tile, n) result;
+          result
   in
-  if n = 0 then
-    tiles
-  else
-    process (n - 1) (transform tiles [])
+  List.map (fun tile -> transform tile n) tiles |> Utils.sum
 
 let _ =
   let tiles = Utils.read_line "input/day11.txt" |> String.split_on_char ' ' |> List.map int_of_string in
   let part1 = process 25 tiles in
-  let cache = Hashtbl.create 100000 in
-  let part2 =
-    List.map
-      (fun n ->
-        match Hashtbl.find_opt cache n with
-        | Some r -> r
-        | None ->
-            let r = process 50 [n] |> List.length in
-            Hashtbl.add cache n r;
-            r)
-      part1
-    |> Utils.sum
-  in
-  Printf.printf "Part 1: %d; Part 2: %d\n" (List.length part1) part2
+  let part2 = process 75 tiles in
+  Printf.printf "Part 1: %d; Part 2: %d\n" part1 part2

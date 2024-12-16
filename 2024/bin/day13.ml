6e41ef72b8861486177cd2d230631795cb499ad0
Keith Gaughan <k@stereochro.me>
2024-12-16 04:26:58 +0000

    2024, day 13, part 2: I can't really claim responsibility for this, as my linear algebra is shot
    Man, the 'find the minimum' thing threw me for a loop, and I spent ages
    trying to remember how to do linear programming, but it wasn't exactly
    going the best.
    
    Then I realised that the equality means it's just a system of linear
    equations, which is much simpler, but still something I haven't done
    since university I stumbled across https://redd.it/1hd7irq, which
    mentioned https://www.youtube.com/watch?v=vXqlIOX2itM, and I sat through
    that to see how to implement the answer.
    
    A few decades ago, I actually knew this stuff... :-(
    
    I got rid of my initial brute force answer, but it's still there in the
    repo history.

--- 2024/bin/day13.ml
+++ 2024/bin/day13.ml
@@ -6,7 +6,7 @@ type line =
 let group lst =
   let rec loop acc lst =
     match lst with
-    | Button (xa, ya) :: Button (xb, yb) :: Prize (xp, yp) :: tl -> loop ((xa, ya, xb, yb, xp, yp) :: acc) tl
+    | Button (a1, a2) :: Button (b1, b2) :: Prize (c1, c2) :: tl -> loop ((a1, a2, b1, b2, c1, c2) :: acc) tl
     | Empty :: tl -> loop acc tl
     | [] -> acc
     | _ -> raise (Invalid_argument "WAT")
@@ -23,29 +23,30 @@ let read_input path =
         Empty)
   |> group
 
-(* I know the proper solution here is linear programming, but it's been so long
-   since I've done that... *)
-let play (xa, ya, xb, yb, xp, yp) =
-  (* the upper limits for how often we can use a given button *)
-  let max_b = Int.min (xp / xb) (yp / yb) in
-  let rec loop nb cost =
-    if nb = 0 then
-      cost
-    else
-      let xpr = xp - (nb * xb)
-      and ypr = yp - (nb * yb) in
-      if xpr mod xa = 0 && ypr mod ya = 0 && xpr / xa = ypr / ya then
-        loop (nb - 1) (Int.min (nb + (3 * (xpr / xa))) cost)
-      else
-        loop (nb - 1) cost
-  in
-  let result = loop max_b Int.max_int in
-  if result = Int.max_int then
-    None
+let make_monsterous (a1, a2, b1, b2, c1, c2) = (a1, a2, b1, b2, 10000000000000 + c1, 10000000000000 + c2)
+
+(* Cramer's rule for solving a series of linear equations, as cogged from
+   https://www.youtube.com/watch?v=vXqlIOX2itM - TBH, I don't entirely
+   understand this anymore, and it's pretty clear that I need to go back and
+   revise how systems of linear equations work.
+
+   Also:
+    * https://en.wikipedia.org/wiki/System_of_linear_equations
+    * https://en.wikipedia.org/wiki/Cramer%27s_rule
+    * https://en.wikipedia.org/wiki/Gaussian_elimination
+*)
+let play (a1, a2, b1, b2, c1, c2) =
+  let determiner = (a1 * b2) - (b1 * a2) in
+  let x = ((c1 * b2) - (b1 * c2)) / determiner
+  and y = ((a1 * c2) - (c1 * a2)) / determiner in
+  if (a1 * x) + (b1 * y) = c1 && (a2 * x) + (b2 * y) = c2 then
+    Some ((3 * x) + y)
   else
-    Some result
+    None
 
 let _ =
   let machines = read_input "input/day13.txt" in
   let part1 = List.filter_map play machines |> Utils.sum in
-  Printf.printf "%d\n" part1
+  Printf.printf "Part 1: %d\n" part1;
+  let part2 = List.map make_monsterous machines |> List.filter_map play |> Utils.sum in
+  Printf.printf "Part 2: %d\n" part2

0679bab7a85791ea0c599ea46d94a262707e8856
Keith Gaughan <k@stereochro.me>
2024-12-18 02:17:18 +0000

    2024, day 12, part 2: it's not elegant, but it works...
    There's almost certainly a more elegant way of discovering if a plot has
    corners, but this'll do for now.
    
    The principal here is that for a given shape, a side should correspond
    to a corner, thus counting corners gives you the number of sides.
    
    A way to improve this would be to take a diagonal and derive the
    adjacent edges from this to determine if it's a concave/convex corner or
    not. I'll do that later when I feel like cleaning the code up.

--- 2024/bin/day12.ml
+++ 2024/bin/day12.ml
@@ -1,6 +1,6 @@
 type plot =
   | Unscanned of char
-  | Scanned of char * int
+  | Scanned of char
 
 let directions = [(0, -1); (0, 1); (1, 0); (-1, 0)]
 
@@ -11,16 +11,17 @@ let read_input path =
    the perimeter. Once that's obtained, the rest of the plots in the region
    can be found to determine its area. *)
 
-let trace_region x y ch id map =
+let trace_region x y ch map =
   let area = ref 0
   and perimeter = ref 0
+  and corners = ref 0
   and pending = Queue.create () in
   let in_bounds x y = x >= 0 && y >= 0 && x < Array.length map.(0) && y < Array.length map in
   let is_perimeter x y =
     if in_bounds x y then
       match map.(y).(x) with
       | Unscanned ch' -> ch' <> ch
-      | Scanned (ch', _) -> ch' <> ch
+      | Scanned ch' -> ch' <> ch
     else
       true
   in
@@ -32,17 +33,43 @@ let trace_region x y ch id map =
     else
       None
   in
+  let count_corners x y =
+    let np = is_perimeter x (y - 1)
+    and sp = is_perimeter x (y + 1)
+    and wp = is_perimeter (x - 1) y
+    and ep = is_perimeter (x + 1) y
+    and nwc = is_perimeter (x - 1) (y - 1)
+    and nec = is_perimeter (x + 1) (y - 1)
+    and swc = is_perimeter (x - 1) (y + 1)
+    and sec = is_perimeter (x + 1) (y + 1) in
+    List.fold_left
+      (fun acc b -> acc + if b then 1 else 0)
+      0
+      [
+        (* convex corners *)
+        np && wp;
+        np && ep;
+        sp && wp;
+        sp && ep;
+        (* concave corners *)
+        nwc && not (np || wp);
+        nec && not (np || ep);
+        swc && not (sp || wp);
+        sec && not (sp || ep);
+      ]
+  in
   let push (x, y) =
     Queue.add (x, y) pending;
-    map.(y).(x) <- Scanned (ch, id);
+    map.(y).(x) <- Scanned ch;
     area := !area + 1;
     perimeter :=
-      List.fold_left (fun acc (dx, dy) -> acc + if is_perimeter (x + dx) (y + dy) then 1 else 0) !perimeter directions
+      List.fold_left (fun acc (dx, dy) -> acc + if is_perimeter (x + dx) (y + dy) then 1 else 0) !perimeter directions;
+    corners := !corners + count_corners x y
   in
   push (x, y);
   let rec loop () =
     if Queue.length pending = 0 then
-      (!area, !perimeter)
+      (!area, !perimeter, !corners)
     else
       let (x', y') = Queue.take pending in
       List.iter (fun (dx, dy) -> try_move (x' + dx) (y' + dy) |> Option.iter push) directions;
@@ -59,8 +86,7 @@ let discover_regions grid =
     for x = 0 to width - 1 do
       match grid.(y).(x) with
       | Unscanned ch ->
-          let (area, perimeter) = trace_region x y ch !id grid in
-          Hashtbl.add regions !id (area, perimeter);
+          Hashtbl.add regions !id (trace_region x y ch grid);
           id := !id + 1
       | _ -> ()
     done
@@ -70,5 +96,6 @@ let discover_regions grid =
 let _ =
   let grid = read_input "input/day12.txt" in
   let regions = discover_regions grid in
-  let part1 = Hashtbl.fold (fun _ (area, perimeter) acc -> acc + (area * perimeter)) regions 0 in
-  Printf.printf "Part 1: %d\n" part1
+  let part1 = Hashtbl.fold (fun _ (area, perimeter, _) acc -> acc + (area * perimeter)) regions 0 in
+  let part2 = Hashtbl.fold (fun _ (area, _, corners) acc -> acc + (area * corners)) regions 0 in
+  Printf.printf "Part 1: %d; Part 2: %d\n" part1 part2

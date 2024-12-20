ba06e3d627dc8d17d39f2779c7f5e2511513c5ec
Keith Gaughan <k@stereochro.me>
2024-12-20 21:37:45 +0000

    2024, day 20, part 2

--- 2024/bin/day20.ml
+++ 2024/bin/day20.ml
@@ -1,12 +1,10 @@
-let nsew = [(1, 0); (-1, 0); (0, 1); (0, -1)]
-
 let trace start endpoint track =
   (* Assumption: S will be surrounded by walls on all but one side *)
   let find_opening (x, y) directions =
     List.filter_map (fun (dx, dy) -> if track.(y + dy).(x + dx) = '#' then None else Some (dx, dy)) directions
     |> List.hd
   in
-  let initial_direction = find_opening start nsew in
+  let initial_direction = find_opening start [(1, 0); (-1, 0); (0, 1); (0, -1)] in
   let rec loop here direction acc distance =
     if here = endpoint then
       (here, distance) :: acc
@@ -17,21 +15,45 @@ let trace start endpoint track =
   in
   loop start initial_direction [] 0
 
-let find_adjacents (x, y) width height trail =
+let find_adjacents (x, y) width height trail jumps =
+  let visited = Hashtbl.create 1000 in
   List.filter_map
     (fun (dx, dy) ->
-      let (x', y') = (x + (dx * 2), y + (dy * 2)) in
-      if x' >= 0 && x < width && y >= 0 && y < height then
-        Utils.IntPairMap.find_opt (x', y') trail
+      let (x', y') = (x + dx, y + dy) in
+      if Hashtbl.mem visited (x', y') then
+        None
+      else if x' >= 0 && x < width && y >= 0 && y < height then
+        match Utils.IntPairMap.find_opt (x', y') trail with
+        | Some d ->
+            Hashtbl.add visited (x', y') true;
+            Some (d - abs dx - abs dy)
+        | None -> None
       else
         None)
-    nsew
+    jumps
 
-let do_part1 width height trail =
+let manhattan_circle i r =
+  let rec loop r offset acc =
+    if offset = 0 then
+      acc
+    else
+      loop r (offset - 1)
+        ((offset, r - offset) :: (r - offset, -offset) :: (-offset, offset - r) :: (offset - r, offset) :: acc)
+  in
+  let rec fill width acc =
+    if width = i then
+      acc
+    else
+      fill (width - 1) (loop width width acc)
+  in
+  fill r []
+
+let race dist threshold width height trail =
+  let jumps = manhattan_circle 1 dist in
   Utils.IntPairMap.fold
     (fun pos distance acc ->
-      find_adjacents pos width height trail
-      |> List.fold_left (fun acc v -> acc + if v - distance - 2 >= 100 then 1 else 0) acc)
+      find_adjacents pos width height trail jumps
+      |> List.fold_left (fun acc v -> acc + if v - distance >= threshold then 1 else 0) acc)
     trail 0
 
 let _ =
@@ -39,5 +61,6 @@ let _ =
   let start = Utils.find_cell (fun ch -> ch = 'S') track |> Option.get in
   let endpoint = Utils.find_cell (fun ch -> ch = 'E') track |> Option.get in
   let points = trace start endpoint track |> List.to_seq |> Utils.IntPairMap.of_seq in
-  let part1 = do_part1 (Array.length track.(0)) (Array.length track) points in
-  Printf.printf "Part 1: %d\n" part1
+  let part1 = race 2 100 (Array.length track.(0)) (Array.length track) points in
+  let part2 = race 20 100 (Array.length track.(0)) (Array.length track) points in
+  Printf.printf "Part 1: %d; Part 2: %d\n" part1 part2

0ee9d7f9afef7af89e0bc8461caa1654b997d32a
Keith Gaughan <k@stereochro.me>
2025-01-12 22:44:29 +0000

    2016: dejank day 4
    This makes better use of the standard library, skips a bunch of
    unnecessary stuff (like the special handling for hyphens), and leans on
    the existing histogram in the hashtable for counts rather than
    constructing a whole new list, which was error prone.

--- 2016/lib/day04.ml
+++ 2016/lib/day04.ml
@@ -3,54 +3,47 @@ let read_entries ic =
     if Scanf.Scanning.end_of_input ic then
       acc
     else
-      Scanf.bscanf ic "%[a-z-]%d[%[a-z]]\n" (fun name num chk ->
-          read ((String.sub name 0 (String.length name - 1), num, chk) :: acc))
+      Scanf.bscanf ic "%[a-z-]%d[%[a-z]]\n" (fun name num chk -> read ((name, num, chk) :: acc))
   in
   read []
 
 let read filename = Scanf.Scanning.open_in filename |> read_entries
-let chars_of_string str = String.to_seq str |> List.of_seq
+
+let make_histogram s =
+  let add tbl ch =
+    (match Hashtbl.find_opt tbl ch with
+    | Some n -> Hashtbl.replace tbl ch (1 + n)
+    | None -> if ch <> '-' then Hashtbl.add tbl ch 1);
+    tbl
+  in
+  String.fold_left add (Hashtbl.create (String.length s)) s
 
 let make_checksum name =
-  let tbl = Hashtbl.create (String.length name) in
-  let add ch =
-    match Hashtbl.find_opt tbl ch with
-    | Some n -> Hashtbl.replace tbl ch (1 + n)
-    | None -> Hashtbl.add tbl ch 1
-  in
-  let comparator (ch1, n1) (ch2, n2) =
-    match compare n1 n2 with
+  let tbl = make_histogram name in
+  let comparator ch1 ch2 =
+    match compare (Hashtbl.find tbl ch1) (Hashtbl.find tbl ch2) with
     | 0 -> compare ch1 ch2
     | x -> -x
   in
-  List.iter (fun ch -> if ch <> '-' then add ch else ()) (chars_of_string name);
-  let histogram = Hashtbl.fold (fun k v acc -> (k, v) :: acc) tbl [] |> List.sort comparator in
-  let untruncated = histogram |> List.map fst |> List.to_seq |> String.of_seq in
-  String.sub untruncated 0 5
+  Hashtbl.to_seq_keys tbl |> List.of_seq |> List.sort comparator
+  |> List.filteri (fun i _ -> i < 5)
+  |> List.to_seq |> String.of_seq
 
 let caesar n input =
-  String.map
-    (fun ch ->
-      if ch = '-' then
-        ' '
-      else
-        char_of_int (((int_of_char ch - 0x61 + n) mod 26) + 0x61))
-    input
+  let rotate ch = ((int_of_char ch - 0x61 + n) mod 26) + 0x61 |> char_of_int in
+  String.map rotate input
 
 let contains s1 s2 =
-  let re = Str.regexp_string s2 in
   try
-    ignore (Str.search_forward re s1 0);
+    ignore (Str.search_forward (Str.regexp_string s2) s1 0);
     true
   with Not_found -> false
 
-let valid_sectors input =
-  List.fold_left (fun acc (n, id, c) -> if c = make_checksum n then (n, id) :: acc else acc) [] input
+let valid_sectors input = List.filter (fun (n, _, c) -> c = make_checksum n) input
 
 let part_one input =
-  valid_sectors input |> List.fold_left (fun acc (_, id) -> acc + id) 0 |> Printf.printf "Part 1: %d\n%!"
+  valid_sectors input |> List.fold_left (fun acc (_, id, _) -> acc + id) 0 |> Printf.printf "Part 1: %d\n%!"
 
 let part_two input =
-  valid_sectors input
-  |> List.fold_left (fun result (n, id) -> if contains (caesar id n) "north" then id else result) 0
-  |> Printf.printf "Part 2: %d\n%!"
+  let (_, id, _) = valid_sectors input |> List.find (fun (n, id, _) -> contains (caesar id n) "north") in
+  Printf.printf "Part 2: %d\n%!" id

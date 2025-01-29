50bd17f8a8dd103a029391d23b7584af6cb53e9d
Keith Gaughan <k@stereochro.me>
2025-01-29 21:21:17 +0000

    2016: clean up, prep

---
+++ 2016/lib/day12.mli
@@ -0,0 +1,3 @@
+val read : string -> Assembunny.Asmb.bytecode array
+val part_one : Assembunny.Asmb.bytecode array -> unit
+val part_two : Assembunny.Asmb.bytecode array -> unit
---
+++ 2016/lib/day13.mli
@@ -0,0 +1,3 @@
+val read : string -> int
+val part_one : int -> unit
+val part_two : int -> unit
